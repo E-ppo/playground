d68991b6d60d52752917c40e369490f35666817b
epppo <amorosso@kakao.com>
2024-10-01 22:39:18 +0900

    chore : reset Css 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
-import Image from "next/image"
-import styles from "./page.module.css"
-import TestContainerClient from "@/containers/test/testContainer.client"
+import Image from 'next/image'
+import styles from './page.module.css'
+import TestContainerClient from '@/containers/test/testContainer.client'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <div className={styles.ctas}>
           <TestContainerClient />
         </div>
