02311a87bad20bef58290071b5a24fd1ef7f5929
epppo <amorosso@kakao.com>
2024-10-03 23:17:52 +0900

    update : 로고 폰트 사이즈 지정

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import styles from './header.module.scss'
 
 const Logo = () => {
-  return <div className={styles.logo}>Eppo Playgroud</div>
+  return (
+    <div className={styles.logo}>
+      <p>Eppo Playgroud</p>
+    </div>
+  )
 }
 
 export default Logo
