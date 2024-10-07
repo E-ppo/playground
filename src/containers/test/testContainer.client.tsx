f9aa81cac74e795216f445fd33374b3b57f75404
epppo <amorosso@kakao.com>
2024-10-07 23:47:32 +0900

    remove : 불필요한 코드 삭제

--- src/containers/test/testContainer.client.tsx
+++ src/containers/test/testContainer.client.tsx
@@ -2,13 +2,11 @@
 import { useTestStore } from '@/states/testStore'
 import styles from './test.module.scss'
 import React from 'react'
-import { FontBold } from '@/public/svgs'
 
 const TestContainerClient = () => {
   const { testStr } = useTestStore()
   return (
     <div className={styles.divContainer}>
-      <FontBold />
       <p>{testStr}</p>
     </div>
   )
