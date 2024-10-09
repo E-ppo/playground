1da6fb95a7b2914b9ecef249944b06cbfe0bde2f
epppo <amorosso@kakao.com>
2024-10-09 21:38:22 +0900

    update : ProfileOverView 컴포넌트 공통으로 사용할 수 있도록 OverView 컴포넌트로 변경 및 수정

---
+++ src/components/overView/index.tsx
@@ -0,0 +1,40 @@
+'use client'
+import Image, { StaticImageData } from 'next/image'
+import React from 'react'
+import styles from './over-view.module.scss'
+import Button from '../button'
+
+interface Props {
+  img?: StaticImageData
+  title: string
+  description: string
+  button?: {
+    text: string
+    onClick: () => void
+  }
+}
+
+const OverView = ({ img, title, description, button }: Props) => {
+  return (
+    <section className={styles[`profile-box`]}>
+      <div>
+        {img && (
+          <Image
+            className={styles[`profile-box-img`]}
+            src={img}
+            alt="character"
+          />
+        )}
+      </div>
+      <div className={styles['contents-box']}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className={styles['button-box']}>
+        {button && <Button variant="secondary" text={button?.text} />}
+      </div>
+    </section>
+  )
+}
+
+export default OverView
