419ef74829e051669fd6ecb5f31162547406276b
epppo <amorosso@kakao.com>
2024-11-17 22:13:08 +0900

    [#7] feat :  로그인 ui 입력 폼 벨리데이션 + 폼데이터 submit + firebase 인증까지 확인하는 useLoginForm 훅 설정

--- src/containers/header/mypage/LoginForm.tsx
+++ src/containers/header/mypage/LoginForm.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+'use clinet'
+
+import React, { useState } from 'react'
 import styles from './mypage.module.scss'
+import { useLoginForm } from '@/hooks'
 
 const LoginForm = () => {
+  const { email, pw, error, onChange, onSubmit } = useLoginForm()
+
   return (
-    <form action="/post" method="POST" className={styles['login-form']}>
+    <form onSubmit={onSubmit} className={styles['login-form']}>
       <h1>로그인</h1>
       <div className={styles['login__item']}>
         <label htmlFor="email">이메일</label>
@@ -11,6 +16,8 @@ const LoginForm = () => {
           type="email"
           name="email"
           id="email"
+          value={email}
+          onChange={onChange}
           required
           placeholder="이메일!!!!!"
         />
@@ -21,17 +28,26 @@ const LoginForm = () => {
           type="password"
           name="password"
           id="password"
+          value={pw}
+          onChange={onChange}
           required
           placeholder="비밀번호!!!!"
         />
       </div>
-      <div className={styles['login__description']}>
-        <p>로그인은 제껍니다</p>
+      <div>
+        <span className={styles['login__description']}>
+          로그인은 제껍니다 :)
+        </span>
+        {error && error?.length > 0 && (
+          <span className={styles['login__description-error']}>{error}</span>
+        )}
       </div>
+
       <div className={styles['login__description']}>
         <input
           type="submit"
           value="로그인"
+          disabled={error?.length > 0}
           className={styles['login__btn--submit']}
         />
       </div>
