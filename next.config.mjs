c36c1ce94ff075e8ee3d83c51cc6bdd232f9352b
epppo <amorosso@kakao.com>
2024-11-22 22:41:48 +0900

    [#9] chore : tiptap 라이브러리 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
+  webpack: (config, { isServer }) => {
+    // SVG 설정
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
+
+    // YJS 설정
+    if (!isServer) {
+      config.resolve.alias['yjs'] = path.resolve(__dirname, 'node_modules/yjs')
+    }
 
     return config
   },
