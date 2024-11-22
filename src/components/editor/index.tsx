9fbfcb10609591301d0d5f37c542c3bde2ed0923
epppo <amorosso@kakao.com>
2024-11-22 22:45:00 +0900

    [#9] feat : 에디터 컴포넌트 기본 외형 구현

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,83 +1,32 @@
 'use client'
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import { useState } from 'react'
+import Toolbar from './Toolbar'
 import styles from './editor.module.scss'
 
-interface EditorProps {
-  initialContent?: string
-  onSave?: (content: string) => void
-}
-
-const Editor = ({ initialContent = '', onSave }: EditorProps) => {
-  const [content, setContent] = useState(initialContent)
-  const editorRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    if (editorRef.current) {
-      editorRef.current.innerHTML = content
-    }
-  }, [])
-
-  const applyFormatting = useCallback((format: string) => {
-    const selection = document.getSelection()
-    const range = selection?.getRangeAt(0)
-
-    const span = document.createElement('span')
-
-    switch (format) {
-      case 'bold':
-        span.style.fontWeight = 'bold'
-        break
-      case 'italic':
-        span.style.fontStyle = 'italic'
-        break
-      case 'underline':
-        span.style.textDecoration = 'underline'
-        break
-    }
-
-    if (range) range.surroundContents(span)
-  }, [])
+const Editor = () => {
+  const [text, setText] = useState('Hello World!')
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: text,
+    onUpdate({ editor }) {
+      setText(editor.getHTML())
+    },
+    editorProps: {
+      attributes: {
+        class:
+          'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl focus:outline-none',
+      },
+    },
+  })
 
   return (
-    <section>
-      <header className={styles.toolbar}>
-        <button onClick={() => applyFormatting('bold')} aria-label="Bold">
-          <strong>B</strong>
-        </button>
-        <button
-          // onClick={() => execCommand('italic')}
-          aria-label="Italic"
-        >
-          <em>I</em>
-        </button>
-        <button
-          // onClick={() => execCommand('underline')}
-          aria-label="Underline"
-        >
-          <u>U</u>
-        </button>
-        <button
-          // onClick={() => execCommand('insertUnorderedList')}
-          aria-label="Bullet List"
-        >
-          •
-        </button>
-        <button
-          // onClick={() => execCommand('insertOrderedList')}
-          aria-label="Numbered List"
-        >
-          1.
-        </button>
-      </header>
-      <main
-        ref={editorRef}
-        className={styles.editableContent}
-        contentEditable
-        aria-label="Editor content"
-        role="textbox"
-        aria-multiline="true"
-      />
-    </section>
+    <div className={styles.editor}>
+      {editor && <Toolbar editor={editor} />}
+      <EditorContent editor={editor} />
+    </div>
   )
 }
 
